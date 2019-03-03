931d1a5bf66bee02641356665d94d4d496979c51
kawashima <nazonohito51@gmail.com>
2019-03-03 15:24:44 +0900

    typo

--- packages/Acme/Application/Exceptions/ApplicationException.php
+++ packages/Acme/Application/Exceptions/ApplicationException.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-namespace App\Exceptions;
+namespace Acme\Application\Exceptions;
 
 abstract class ApplicationException extends \RuntimeException
 {
--- packages/Acme/Application/Exceptions/SaveEntityException.php
+++ packages/Acme/Application/Exceptions/SaveEntityException.php
@@ -3,8 +3,6 @@ declare(strict_types=1);
 
 namespace Acme\Application\Exceptions;
 
-use App\Exceptions\ApplicationException;
-
 class SaveEntityException extends ApplicationException
 {
 }
